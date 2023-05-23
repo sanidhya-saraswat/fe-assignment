99fc261b9d473cfc05ac77b13715c2f221b4da3a
sanidhya-saraswat <sanidhya.saraswat@gmail.com>
2023-05-23 14:22:25 +0530

    Revert "Initial commit"
    This reverts commit 24af0edecc7c98e0f0448ee111833d126fd43040.

--- ecom/src/constants.js
+++
@@ -1,5 +0,0 @@
-export const PRODUCT_LISTING_LIMIT = 6;
-export const DEFAULT_MIN_PRICE = 0;
-export const DEFAULT_MAX_PRICE = 50000;
-export const DEFAULT_RATING = 0;
-export const SNACKBAR_DEFAULT_TIMEOUT = 3000;
